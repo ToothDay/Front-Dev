c816241663549740e5279c6bd302868c52d12ef3
MoonSsunny <moonsun116@naver.com>
2024-06-21 00:44:01 +0900

    초기셋팅

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/page.module.scss";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>test page</h1>
+    </main>
+  );
+};
+
+export default Home;
