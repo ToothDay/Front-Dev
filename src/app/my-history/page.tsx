c3d8472645c6992eeae211273880aac097a04612
MoonSsunny <moonsun116@naver.com>
2024-07-05 08:45:56 +0900

    [13] feat : 나의 진료기록 및 카드리스트 구현

--- src/app/my-history/page.tsx
+++ src/app/my-history/page.tsx
@@ -1,11 +1,20 @@
 import styles from "@/app/my-history/page.module.scss";
 import Header from "@/components/common/Header";
+import HistoryCard from "@/components/common/HistoryCard";
 
 const MyHistory = () => {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
         <Header title="나의 진료 기록" />
+      </div>
+      <div className={styles.cardList}>
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
       </div>
     </main>
   );
