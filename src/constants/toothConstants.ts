2ac6e6c0c2d85978c9da2f5e24200402cec6551c
MoonSsunny <moonsun116@naver.com>
2024-07-07 16:34:28 +0900

    [23] feat : 상수 처리

---
+++ src/constants/toothConstants.ts
@@ -0,0 +1,44 @@
+type LeftToothType = {
+  number: number;
+  name: string;
+};
+
+export const LEFT_TOP: LeftToothType[] = [
+  { number: 1, name: "왼쪽 1번 앞니" },
+  { number: 2, name: "왼쪽 2번 앞니" },
+  { number: 3, name: "왼쪽 3번 송곳니" },
+  { number: 4, name: "왼쪽 4번 작은어금니" },
+  { number: 5, name: "왼쪽 5번 작은어금니" },
+  { number: 6, name: "왼쪽 6번 큰어금니" },
+  { number: 7, name: "왼쪽 7번 큰어금니" }
+];
+
+export const LEFT_BOTTOM: LeftToothType[] = [
+  { number: 1, name: "왼쪽 1번 중절치" },
+  { number: 2, name: "왼쪽 2번 축절치" },
+  { number: 3, name: "왼쪽 3번 건니" },
+  { number: 4, name: "왼쪽 4번 소구치" },
+  { number: 5, name: "왼쪽 5번 소구치" },
+  { number: 6, name: "왼쪽 6번 대구치" },
+  { number: 7, name: "왼쪽 7번 대구치" }
+];
+
+export const RIGHT_TOP: LeftToothType[] = [
+  { number: 1, name: "오른쪽 1번 앞니" },
+  { number: 2, name: "오른쪽 2번 앞니" },
+  { number: 3, name: "오른쪽 3번 송곳니" },
+  { number: 4, name: "오른쪽 4번 작은어금니" },
+  { number: 5, name: "오른쪽 5번 작은어금니" },
+  { number: 6, name: "오른쪽 6번 큰어금니" },
+  { number: 7, name: "오른쪽 7번 큰어금니" }
+];
+
+export const RIGHT_BOTTOM: LeftToothType[] = [
+  { number: 1, name: "오른쪽 1번 중절치" },
+  { number: 2, name: "오른쪽 2번 축절치" },
+  { number: 3, name: "오른쪽 3번 건니" },
+  { number: 4, name: "오른쪽 4번 소구치" },
+  { number: 5, name: "오른쪽 5번 소구치" },
+  { number: 6, name: "오른쪽 6번 대구치" },
+  { number: 7, name: "오른쪽 7번 대구치" }
+];
