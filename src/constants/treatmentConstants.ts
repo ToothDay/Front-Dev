6f894f665a9f7c2b2b4876d69704371b73bc3a78
MoonSsunny <moonsun116@naver.com>
2024-07-10 09:10:27 +0900

    [29] feat : 공통 치료 변수 추가

---
+++ src/constants/treatmentConstants.ts
@@ -0,0 +1,14 @@
+type Treatment = {
+  id: number;
+  name: string;
+};
+
+export const treatmentList: Treatment[] = [
+  { id: 1, name: "잇몸" },
+  { id: 2, name: "스케일링" },
+  { id: 3, name: "레진" },
+  { id: 4, name: "인레이" },
+  { id: 5, name: "크라운" },
+  { id: 6, name: "신경" },
+  { id: 7, name: "임플란트" }
+];
