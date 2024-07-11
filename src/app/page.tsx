bad5d33255aa5ada3531b17fde0e730026eaab23
MoonSsunny <moonsun116@naver.com>
2024-07-11 20:55:24 +0900

    [35] feat : 첫 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import styles from "@/app/page.module.scss";
 const Home = () => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>test page</h1>
+      <h1 className={styles.title}>
+        <img
+          src="/main/logo-img.png"
+          alt="ToothDay-logo"
+          className={styles.logo}
+        />
+        <img src="/main/logo-text.png" alt="ToothDay" className={styles.text} />
+      </h1>
+      <img
+        src="/main/main-top.png"
+        alt="background"
+        className={styles.backTop}
+      />
+      <div className={styles.backBottom}></div>
+      <div className={styles.mainButton}>
+        <button type="button" className={styles.button}>
+          서비스 둘러보기
+        </button>
+        <button type="button" className={styles.button}>
+          바로 로그인 하기
+        </button>
+      </div>
     </main>
   );
 };
