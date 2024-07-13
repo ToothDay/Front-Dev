ec6141e647d3f204a00ea692919e37eb6a1e5077
MoonSsunny <moonsun116@naver.com>
2024-07-14 08:44:55 +0900

    [42] feat : 모달 관련 오픈 트랜지션

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "./Modal.module.scss";
 import { useModalStore } from "@/stores/modal";
 
@@ -17,16 +17,25 @@ const Modal = ({ children }: PropsModal) => {
   };
 
   return (
-    <div
-      className={[styles.wrapper, isOpen ? styles.open : ""].join(" ")}
-      onClick={handleModal}
-    >
-      {isOpen && (
-        <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-          {children}
-        </div>
-      )}
-    </div>
+    <AnimatePresence>
+      <div
+        className={[styles.wrapper, isOpen ? styles.open : ""].join(" ")}
+        onClick={handleModal}
+      >
+        {isOpen && (
+          <motion.div
+            className={styles.modal}
+            onClick={(e) => e.stopPropagation()}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            {children}
+          </motion.div>
+        )}
+      </div>
+    </AnimatePresence>
   );
 };
 
