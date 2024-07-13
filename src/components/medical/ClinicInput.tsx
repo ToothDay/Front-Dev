55446c22c3f2d7ff98a509acd85f3999033f2c49
MoonSsunny <moonsun116@naver.com>
2024-07-13 09:43:04 +0900

    [36]feat : 안쓰는 변수 정리

--- src/components/medical/ClinicInput.tsx
+++ src/components/medical/ClinicInput.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import styles from "./ClinicInput.module.scss";
 
