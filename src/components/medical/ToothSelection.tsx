eea942d4378d425d4db35512cc2e9df68f045696
MoonSsunny <moonsun116@naver.com>
2024-07-14 18:08:13 +0900

    [42] feat : 치료 설정 조건 추가 스케일링, 잇몸의 경우만 있을 경우 안보이도록 추가

--- src/components/medical/ToothSelection.tsx
+++ src/components/medical/ToothSelection.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Tooth from "../tooth/Tooth";
 import styles from "./ToothSelection.module.scss";
+import { useTreatmentType } from "../../stores/medicalWrite";
 
 type ToothSide = {
   value: "left" | "right";
@@ -9,7 +10,9 @@ type ToothSide = {
 };
 
 const ToothSelection = () => {
+  const { treatmentType } = useTreatmentType();
   const [toothSelect, setToothSelect] = useState<"left" | "right">("left");
+  const [isDisplay, setIsDisplay] = useState<boolean>(false);
   const toothSide: ToothSide[] = [
     {
       value: "left",
@@ -20,52 +23,62 @@ const ToothSelection = () => {
       name: "오른쪽"
     }
   ];
+  useEffect(() => {
+    const shouldDisplay = !treatmentType.every(
+      (treatment) => treatment.name === "스케일링" || treatment.name === "잇몸"
+    );
+    setIsDisplay(shouldDisplay);
+  }, [treatmentType]);
 
   return (
-    <motion.div
-      className={styles.writeWrapper}
-      initial={{ opacity: 0, x: 100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-    >
-      <label className={styles.writeLabel}>
-        치아를 선택해서 받은 치료를 <br /> 설정해 주세요.
-      </label>
-      <span className={styles.helperText}>
-        스케일링과 잇몸치료는 설정에서 제외됩니다.
-      </span>
-      <div className={styles.toothItem}>
-        <div className={styles.toothSelect}>
-          {toothSide.map((side) => (
-            <button
-              type="button"
-              key={side.value}
-              className={[
-                styles.treatmentButton,
-                side.value === toothSelect ? styles.selected : ""
-              ].join(" ")}
-              onClick={() => setToothSelect(side.value)}
-            >
-              {side.name}
-            </button>
-          ))}
-        </div>
-        <div className={styles.toothImg}>
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={toothSelect}
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: -100 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Tooth location={toothSelect} />
-            </motion.div>
-          </AnimatePresence>
-        </div>
-      </div>
-    </motion.div>
+    <>
+      {isDisplay && (
+        <motion.div
+          className={styles.writeWrapper}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+          <label className={styles.writeLabel}>
+            치아를 선택해서 받은 치료를 <br /> 설정해 주세요.
+          </label>
+          <span className={styles.helperText}>
+            스케일링과 잇몸치료는 설정에서 제외됩니다.
+          </span>
+          <div className={styles.toothItem}>
+            <div className={styles.toothSelect}>
+              {toothSide.map((side) => (
+                <button
+                  type="button"
+                  key={side.value}
+                  className={[
+                    styles.treatmentButton,
+                    side.value === toothSelect ? styles.selected : ""
+                  ].join(" ")}
+                  onClick={() => setToothSelect(side.value)}
+                >
+                  {side.name}
+                </button>
+              ))}
+            </div>
+            <div className={styles.toothImg}>
+              <AnimatePresence mode="wait">
+                <motion.div
+                  key={toothSelect}
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -100 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <Tooth location={toothSelect} />
+                </motion.div>
+              </AnimatePresence>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </>
   );
 };
 
