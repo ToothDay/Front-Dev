37a40374c1e6002124e04ab880642f280d69a1d0
MoonSsunny <moonsun116@naver.com>
2024-07-14 19:30:52 +0900

    [42] feat :  치료 선택 관련 모달창 수정

--- src/components/medical/TreatmentSelection.tsx
+++ src/components/medical/TreatmentSelection.tsx
@@ -10,16 +10,11 @@ import { useTreatmentType } from "@/stores/medicalWrite";
 const TreatmentSelection = () => {
   const { openModal } = useModalStore();
 
-  const [treatmentName, setTreatmentName] = useState<string>("");
-  const [treatmentId, setTreatmentId] = useState<number>(0);
   const { treatmentType, updateOrAddTreatmentType } = useTreatmentType();
 
   const handleTreatmentClick = (id: number, name: string) => {
-    setTreatmentName(name);
-    setTreatmentId(id);
-
     if (id !== 1 && id !== 2) {
-      openModal();
+      openModal(<TreatmentAddModal treatmentName={name} treatmentId={id} />);
     } else {
       if (
         treatmentType.find((treatment) => treatment.name === name)?.number === 1
@@ -56,51 +51,43 @@ const TreatmentSelection = () => {
   };
 
   return (
-    <>
-      <motion.div
-        className={styles.writeWrapper}
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: { opacity: 0, x: 100 },
-          visible: { opacity: 1, x: 0 },
-          exit: { opacity: 0, x: -100 }
-        }}
-        transition={{ duration: 1.0, ease: "easeInOut" }}
-      >
-        <label className={styles.writeLabel}>어떤 치료를 받으셨나요?</label>
-        <span className={styles.helperText}>중복 선택이 가능해요.</span>
-        <div className={styles.treatmentList}>
-          {TREATMENT_LIST.map((treatment) => (
-            <button
-              type="button"
-              key={treatment.id}
-              className={[
-                styles.treatmentButton,
-                checkTreatment(treatment.name) ? styles.selected : ""
-              ].join(" ")}
-              onClick={() => {
-                handleTreatmentClick(treatment.id, treatment.name);
-              }}
-            >
-              {treatment.name}
-              {isDisplayNumber(
-                treatment.name,
-                checkTreatmentNumber(treatment.name)
-              )
-                ? ` ${checkTreatmentNumber(treatment.name)}개 `
-                : ""}
-            </button>
-          ))}
-        </div>
-      </motion.div>
-      <Modal>
-        <TreatmentAddModal
-          treatmentName={treatmentName}
-          treatmentId={treatmentId}
-        />
-      </Modal>
-    </>
+    <motion.div
+      className={styles.writeWrapper}
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0, x: 100 },
+        visible: { opacity: 1, x: 0 },
+        exit: { opacity: 0, x: -100 }
+      }}
+      transition={{ duration: 1.0, ease: "easeInOut" }}
+    >
+      <label className={styles.writeLabel}>어떤 치료를 받으셨나요?</label>
+      <span className={styles.helperText}>중복 선택이 가능해요.</span>
+      <div className={styles.treatmentList}>
+        {TREATMENT_LIST.map((treatment) => (
+          <button
+            type="button"
+            key={treatment.id}
+            className={[
+              styles.treatmentButton,
+              checkTreatment(treatment.name) ? styles.selected : ""
+            ].join(" ")}
+            onClick={() => {
+              handleTreatmentClick(treatment.id, treatment.name);
+            }}
+          >
+            {treatment.name}
+            {isDisplayNumber(
+              treatment.name,
+              checkTreatmentNumber(treatment.name)
+            )
+              ? ` ${checkTreatmentNumber(treatment.name)}개 `
+              : ""}
+          </button>
+        ))}
+      </div>
+    </motion.div>
   );
 };
 
