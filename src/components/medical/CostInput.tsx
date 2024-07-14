a32ae38ca337e118ba13921c3dbb0383bbc69c5c
MoonSsunny <moonsun116@naver.com>
2024-07-14 17:24:39 +0900

    [42] feat : 지불 비용 필드 갯수만큼 생성하도록 수정

--- src/components/medical/CostInput.tsx
+++ src/components/medical/CostInput.tsx
@@ -1,7 +1,28 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import styles from "./CostInput.module.scss";
+import { useTreatmentType } from "../../stores/medicalWrite";
+import { useEffect, useState } from "react";
 
 const CostInput = () => {
+  const { treatmentType } = useTreatmentType();
+  const [costList, setCostList] = useState<string[]>([]);
+
+  const checkTreatmentCost = () => {
+    const newCostList: string[] = [];
+    treatmentType.forEach((item) => {
+      if (item.number > 0) {
+        for (let i = 0; i < item.number; i++) {
+          newCostList.push(item.name);
+        }
+      }
+    });
+    setCostList(newCostList);
+  };
+
+  useEffect(() => {
+    checkTreatmentCost();
+  }, [treatmentType]);
+
   return (
     <motion.div
       className={styles.writeWrapper}
@@ -16,6 +37,33 @@ const CostInput = () => {
       <span className={styles.helperText}>
         각각의 치료에 해당하는 비용을 입력해 주세요.
       </span>
+      <div className={styles.cost}>
+        <AnimatePresence>
+          {costList.map((item, index) => (
+            <motion.div
+              className={styles.costInput}
+              key={index}
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <label className={styles.costLabel}>{item}</label>
+              <div className={styles.costInputBox}>
+                <input
+                  type="text"
+                  className={styles.costBox}
+                  placeholder="숫자만 입력하세요."
+                />
+                <button type="button" className={styles.deleteCost}>
+                  삭제
+                </button>
+              </div>
+              <span className={styles.costUnit}> 원</span>
+            </motion.div>
+          ))}{" "}
+        </AnimatePresence>
+      </div>
     </motion.div>
   );
 };
