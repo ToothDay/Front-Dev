3890cd243e7a15bb6619c3779f45849a0bfbc924
MoonSsunny <moonsun116@naver.com>
2024-07-14 22:16:54 +0900

    [42] feat : 치아 Id 값 추가

--- src/constants/toothConstants.ts
+++ src/constants/toothConstants.ts
@@ -1,45 +1,56 @@
 type LeftToothType = {
+  id: number;
   number: number;
   name: string;
   icon: string;
 };
 
 export const LEFT_TOP: LeftToothType[] = [
-  { number: 1, name: "왼쪽 1번 앞니", icon: "/tooth/top-1.svg" },
-  { number: 2, name: "왼쪽 2번 앞니", icon: "/tooth/top-2.svg" },
-  { number: 3, name: "왼쪽 3번 송곳니", icon: "/tooth/top-3.svg" },
-  { number: 4, name: "왼쪽 4번 작은어금니", icon: "/tooth/top-4.svg" },
-  { number: 5, name: "왼쪽 5번 작은어금니", icon: "/tooth/top-5.svg" },
-  { number: 6, name: "왼쪽 6번 큰어금니", icon: "/tooth/top-6.svg" },
-  { number: 7, name: "왼쪽 7번 큰어금니", icon: "/tooth/top-7.svg" }
+  { id: 1, number: 1, name: "왼쪽 1번 앞니", icon: "/tooth/top-1.svg" },
+  { id: 2, number: 2, name: "왼쪽 2번 앞니", icon: "/tooth/top-2.svg" },
+  { id: 3, number: 3, name: "왼쪽 3번 송곳니", icon: "/tooth/top-3.svg" },
+  { id: 4, number: 4, name: "왼쪽 4번 작은어금니", icon: "/tooth/top-4.svg" },
+  { id: 5, number: 5, name: "왼쪽 5번 작은어금니", icon: "/tooth/top-5.svg" },
+  { id: 6, number: 6, name: "왼쪽 6번 큰어금니", icon: "/tooth/top-6.svg" },
+  { id: 7, number: 7, name: "왼쪽 7번 큰어금니", icon: "/tooth/top-7.svg" }
 ];
 
 export const LEFT_BOTTOM: LeftToothType[] = [
-  { number: 1, name: "왼쪽 1번 중절치", icon: "/tooth/bottom-1.svg" },
-  { number: 2, name: "왼쪽 2번 축절치", icon: "/tooth/bottom-2.svg" },
-  { number: 3, name: "왼쪽 3번 건니", icon: "/tooth/bottom-3.svg" },
-  { number: 4, name: "왼쪽 4번 소구치", icon: "/tooth/bottom-4.svg" },
-  { number: 5, name: "왼쪽 5번 소구치", icon: "/tooth/bottom-5.svg" },
-  { number: 6, name: "왼쪽 6번 대구치", icon: "/tooth/bottom-6.svg" },
-  { number: 7, name: "왼쪽 7번 대구치", icon: "/tooth/bottom-7.svg" }
+  { id: 8, number: 1, name: "왼쪽 1번 중절치", icon: "/tooth/bottom-1.svg" },
+  { id: 9, number: 2, name: "왼쪽 2번 축절치", icon: "/tooth/bottom-2.svg" },
+  { id: 10, number: 3, name: "왼쪽 3번 건니", icon: "/tooth/bottom-3.svg" },
+  { id: 11, number: 4, name: "왼쪽 4번 소구치", icon: "/tooth/bottom-4.svg" },
+  { id: 12, number: 5, name: "왼쪽 5번 소구치", icon: "/tooth/bottom-5.svg" },
+  { id: 13, number: 6, name: "왼쪽 6번 대구치", icon: "/tooth/bottom-6.svg" },
+  { id: 14, number: 7, name: "왼쪽 7번 대구치", icon: "/tooth/bottom-7.svg" }
 ];
 
 export const RIGHT_TOP: LeftToothType[] = [
-  { number: 1, name: "오른쪽 1번 앞니", icon: "/tooth/top-1.svg" },
-  { number: 2, name: "오른쪽 2번 앞니", icon: "/tooth/top-2.svg" },
-  { number: 3, name: "오른쪽 3번 송곳니", icon: "/tooth/top-3.svg" },
-  { number: 4, name: "오른쪽 4번 작은어금니", icon: "/tooth/top-4.svg" },
-  { number: 5, name: "오른쪽 5번 작은어금니", icon: "/tooth/top-5.svg" },
-  { number: 6, name: "오른쪽 6번 큰어금니", icon: "/tooth/top-6.svg" },
-  { number: 7, name: "오른쪽 7번 큰어금니", icon: "/tooth/top-7.svg" }
+  { id: 21, number: 1, name: "오른쪽 1번 앞니", icon: "/tooth/top-1.svg" },
+  { id: 20, number: 2, name: "오른쪽 2번 앞니", icon: "/tooth/top-2.svg" },
+  { id: 19, number: 3, name: "오른쪽 3번 송곳니", icon: "/tooth/top-3.svg" },
+  {
+    id: 18,
+    number: 4,
+    name: "오른쪽 4번 작은어금니",
+    icon: "/tooth/top-4.svg"
+  },
+  {
+    id: 17,
+    number: 5,
+    name: "오른쪽 5번 작은어금니",
+    icon: "/tooth/top-5.svg"
+  },
+  { id: 16, number: 6, name: "오른쪽 6번 큰어금니", icon: "/tooth/top-6.svg" },
+  { id: 15, number: 7, name: "오른쪽 7번 큰어금니", icon: "/tooth/top-7.svg" }
 ];
 
 export const RIGHT_BOTTOM: LeftToothType[] = [
-  { number: 1, name: "오른쪽 1번 중절치", icon: "/tooth/bottom-1.svg" },
-  { number: 2, name: "오른쪽 2번 축절치", icon: "/tooth/bottom-2.svg" },
-  { number: 3, name: "오른쪽 3번 건니", icon: "/tooth/bottom-3.svg" },
-  { number: 4, name: "오른쪽 4번 소구치", icon: "/tooth/bottom-4.svg" },
-  { number: 5, name: "오른쪽 5번 소구치", icon: "/tooth/bottom-5.svg" },
-  { number: 6, name: "오른쪽 6번 대구치", icon: "/tooth/bottom-6.svg" },
-  { number: 7, name: "오른쪽 7번 대구치", icon: "/tooth/bottom-7.svg" }
+  { id: 28, number: 1, name: "오른쪽 1번 중절치", icon: "/tooth/bottom-1.svg" },
+  { id: 27, number: 2, name: "오른쪽 2번 축절치", icon: "/tooth/bottom-2.svg" },
+  { id: 26, number: 3, name: "오른쪽 3번 건니", icon: "/tooth/bottom-3.svg" },
+  { id: 25, number: 4, name: "오른쪽 4번 소구치", icon: "/tooth/bottom-4.svg" },
+  { id: 24, number: 5, name: "오른쪽 5번 소구치", icon: "/tooth/bottom-5.svg" },
+  { id: 23, number: 6, name: "오른쪽 6번 대구치", icon: "/tooth/bottom-6.svg" },
+  { id: 22, number: 7, name: "오른쪽 7번 대구치", icon: "/tooth/bottom-7.svg" }
 ];
