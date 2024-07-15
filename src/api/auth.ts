c487c8a9925e3efeb8bc298408618c9a6d5ddfc2
MoonSsunny <moonsun116@naver.com>
2024-07-15 22:04:53 +0900

    [41] feat: 로컬에서 쿠키설정으로 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,9 @@
 export const getToken = () => localStorage.getItem("jwtToken");
 
 export const setToken = (token: string) => {
-  localStorage.setItem("jwtToken", token);
+  document.cookie = `jwtToken=${token}; path=/; secure; httponly`;
 };
 
 export const removeToken = () => {
-  localStorage.removeItem("jwtToken");
+  document.cookie = "jwtToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
 };
