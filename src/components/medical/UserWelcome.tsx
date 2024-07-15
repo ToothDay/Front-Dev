c46af30d3bf96ddd70b3ad5b8551233c83f3a2c9
MoonSsunny <moonsun116@naver.com>
2024-07-16 00:41:50 +0900

    [41] feat : 첫페이지 컴포넌트 분리작업

---
+++ src/components/medical/UserWelcome.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useUserStore } from "@/stores/user";
+import styles from "./UserWelcome.module.scss";
+import Link from "next/link";
+
+const UserWelcome = () => {
+  const { userProfile } = useUserStore();
+  console.log(userProfile);
+  return (
+    <section className={styles.medicalSection}>
+      <div className={styles.myMedical}>
+        <div className={styles.medicalText}>
+          <span className={styles.title}>
+            안녕하세요 {userProfile?.name} 님!
+          </span>
+          <div className={styles.noDataText}>
+            <span className={styles.text}>최근 진료기록이</span>
+            <span className={styles.text}>아직 없습니다.</span>
+          </div>
+        </div>
+        <Link href="/medical/write">
+          <button type="button" className={styles.recordButton}>
+            진료 기록하러 가기
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default UserWelcome;
