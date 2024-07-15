c6626b0359bdf08b654f1df66453c94af23e7735
MoonSsunny <moonsun116@naver.com>
2024-07-16 00:41:04 +0900

    [41] feat : 첫페이지 컴포넌트 분리 작업

--- src/app/medical/page.tsx
+++ src/app/medical/page.tsx
@@ -1,30 +1,15 @@
-import Header from "@/components/common/Header";
 import styles from "./page.module.scss";
 import Tab from "@/components/common/Tab";
 import HistoryCard from "@/components/common/HistoryCard";
 import TreatmentSwiper from "@/components/common/TreatmentSwiper";
 import Link from "next/link";
+import UserWelcome from "@/components/medical/UserWelcome";
 
 const MedicalPage = () => {
   return (
     <main className={styles.main}>
       <Tab pageType="page" initialActiveTab="진료기록" />
-      <section className={styles.medicalSection}>
-        <div className={styles.myMedical}>
-          <div className={styles.medicalText}>
-            <span className={styles.title}>안녕하세요 님!</span>
-            <div className={styles.noDataText}>
-              <span className={styles.text}>최근 진료기록이</span>
-              <span className={styles.text}>아직 없습니다.</span>
-            </div>
-          </div>
-          <Link href="/medical/write">
-            <button type="button" className={styles.recordButton}>
-              진료 기록하러 가기
-            </button>
-          </Link>
-        </div>
-      </section>
+      <UserWelcome />
       <section className={styles.medicalRecentlySection}>
         <div className={styles.titleWrapper}>
           <span className={styles.wrapperTitle}>최근 진료 기록</span>
