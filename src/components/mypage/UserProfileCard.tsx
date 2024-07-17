8792f890f4ec47bd16f776c812c2ecd139902ec3
MoonSsunny <moonsun116@naver.com>
2024-07-17 23:26:24 +0900

    [48] feat : next image로 이미지가 없을때 데이터 처리

---
+++ src/components/mypage/UserProfileCard.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import styles from "./UserProfileCard.module.scss";
+import { useUserStore } from "@/stores/user";
+import Image from "next/image";
+import { useState } from "react";
+
+const UserProfileCard = () => {
+  const { userProfile } = useUserStore();
+  const { username, profileImageUrl } = userProfile;
+  const [profileImage, setProfileImage] = useState<string>(profileImageUrl);
+  return (
+    <article className={styles.user}>
+      <Image
+        src={profileImage}
+        alt="profile"
+        width={83}
+        height={83}
+        className={styles.profileImg}
+        loading="lazy"
+        placeholder="blur"
+        onError={(e) => {
+          setProfileImage("/profile.svg");
+        }}
+        blurDataURL="/profile.svg"
+      />
+      <div className={styles.userDetail}>
+        <span className={styles.userName}>{username || "-"}</span>
+        <Link href="/my-page/edit">
+          <button type="button" className={styles.profileButton}>
+            프로필 편집
+          </button>
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default UserProfileCard;
