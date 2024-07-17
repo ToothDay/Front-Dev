d3764f79826fcbed1e492d984ba53e484304c24b
MoonSsunny <moonsun116@naver.com>
2024-07-18 01:02:17 +0900

    [41] feat : 토큰이 있는 경우에만 유저정보 불러오도록 설정

--- src/components/UserProfileProvider.tsx
+++ src/components/UserProfileProvider.tsx
@@ -5,34 +5,39 @@ import { useUserStore } from "@/stores/user";
 import { useQuery } from "react-query";
 import Loading from "@/app/loading";
 import Error from "./error/Error";
+import Cookies from "js-cookie";
 
 const UserProfileProvider = ({ children }: { children: React.ReactNode }) => {
   const { setUserProfile } = useUserStore();
   const router = useRouter();
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: "userProfile",
-    queryFn: fetchUserProfile,
-    onSuccess: (data) => {
-      const profile = {
-        id: data.id,
-        email: data.email,
-        profileImageUrl: data.profileImageUrl,
-        username: data.username
-      };
-      setUserProfile(profile);
-    },
-    onError: (error) => {
-      console.error("Failed to fetch user profile", error);
+  const token = Cookies.get("jwtToken");
+
+  if (token) {
+    const { data, error, isLoading } = useQuery({
+      queryKey: "userProfile",
+      queryFn: fetchUserProfile,
+      onSuccess: (data) => {
+        const profile = {
+          id: data.id,
+          email: data.email,
+          profileImageUrl: data.profileImageUrl,
+          username: data.username
+        };
+        setUserProfile(profile);
+      },
+      onError: (error) => {
+        console.error("Failed to fetch user profile", error);
+      }
+    });
+
+    if (isLoading) {
+      return <Loading />;
     }
-  });
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <Error errorType={"error"} />;
+    if (error) {
+      return <Error errorType={"error"} />;
+    }
   }
 
   return <>{children}</>;
