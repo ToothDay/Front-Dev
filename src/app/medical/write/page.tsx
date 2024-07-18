3818ca0ecffeff75b9d604f2f9b232e1fc37ca83
MoonSsunny <moonsun116@naver.com>
2024-07-18 23:46:14 +0900

    [53] feat : 안쓰는 변수 삭제

--- src/app/medical/write/page.tsx
+++ src/app/medical/write/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/common/Header";
 import styles from "./page.module.scss";
-import { TREATMENT_LIST } from "@/constants/treatmentConstants";
 import MedicalWrite from "@/components/medical/MedicalWrite";
 
 const MedicalWritePage = () => {
