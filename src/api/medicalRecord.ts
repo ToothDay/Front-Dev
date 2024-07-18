ed4c9e5ed1f8fd9e7618ecb562a8ceb9c4936246
MoonSsunny <moonsun116@naver.com>
2024-07-18 22:55:28 +0900

    [53] feat : api 수정

--- src/api/medicalRecord.ts
+++ src/api/medicalRecord.ts
@@ -1,6 +1,12 @@
 import axiosTooth from "./api";
+import { SaveParams } from "@/components/medical/MedicalWrite";
 
 export const searchDentist = async (query: string) => {
   const response = await axiosTooth.get(`/dentists/search?query=${query}`);
+  return response.data;
+};
+
+export const saveMyDentist = async (params: SaveParams) => {
+  const response = await axiosTooth.post(`/visit`, params);
   return response.data;
 };
