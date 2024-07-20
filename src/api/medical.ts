41dad6bde840eccde562b30be19834f21eafec33
MoonSsunny <moonsun116@naver.com>
2024-07-20 23:34:25 +0900

    [56] feat : api 맵핑

--- src/api/medical.ts
+++ src/api/medical.ts
@@ -20,7 +20,42 @@ export type VisitData = {
   shared: boolean;
 };
 
+export type VisitDetail = {
+  dentistId: number;
+  dentistName: string;
+  dentistAddress: string;
+  visitDate: string;
+  treatmentList: TreatmentItem[];
+  totalAmount: number;
+  writtenByCurrentUser: boolean;
+  visitID?: number;
+  userID?: number;
+  profileImageUrl?: string;
+  shared?: boolean;
+};
+
+export type VisitMyDetail = Omit<
+  VisitDetail,
+  "visitID" | "userID" | "profileImageUrl" | "shared"
+>;
+
 export const fetchVisitData = async (): Promise<VisitData[]> => {
   const response = await axiosServer.get<VisitData[]>(`/visit`);
+  return response.data;
+};
+
+export const fetchOtherMedicalDetail = async (
+  visitId: string
+): Promise<VisitDetail> => {
+  const response = await axiosServer.get<VisitDetail>(`/visit/${visitId}`);
+  return response.data;
+};
+
+export const fetchMyMedicalDetail = async (
+  visitId: string
+): Promise<VisitMyDetail> => {
+  const response = await axiosServer.get<VisitMyDetail>(
+    `/mypage/visit/${visitId}`
+  );
   return response.data;
 };
--- src/app/medical/detail/[id]/page.tsx
+++ src/app/medical/detail/[id]/page.tsx
@@ -1,60 +1,113 @@
 import Header from "@/components/common/Header";
 import styles from "./page.module.scss";
 import ToothSelectSelection from "@/components/tooth/ToothSelectSection";
+import {
+  VisitDetail,
+  VisitMyDetail,
+  fetchMyMedicalDetail,
+  fetchOtherMedicalDetail
+} from "@/api/medical";
 
-const MedicalDetail = () => {
+type PropsPage = {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    type: string;
+  };
+};
+
+type CategoryList = {
+  category: string;
+  count: number;
+};
+
+const getMyMedicalDetail = async (
+  id: string
+): Promise<VisitMyDetail | undefined> => {
+  try {
+    const response = await fetchMyMedicalDetail(id);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getOtherMedicalDetail = async (
+  id: string
+): Promise<VisitDetail | undefined> => {
+  try {
+    const response = await fetchOtherMedicalDetail(id);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const MedicalDetail = async ({ params, searchParams }: PropsPage) => {
+  let data: VisitMyDetail | VisitDetail | undefined;
+  if (searchParams.type === "me") {
+    data = await getMyMedicalDetail(params.id);
+  } else {
+    data = await getOtherMedicalDetail(params.id);
+  }
+
+  if (!data) {
+    return "-";
+  }
+
+  const categoryList: CategoryList[] = data.treatmentList.reduce<
+    CategoryList[]
+  >((acc, item) => {
+    const index = acc.findIndex(
+      (category) => category.category === item.category
+    );
+    if (index === -1) {
+      acc.push({ category: item.category, count: 1 });
+    } else {
+      acc[index].count += 1;
+    }
+    return acc;
+  }, []);
+
   return (
     <main className={styles.main}>
       <Header />
       <section className={styles.infoSection}>
-        <div className={styles.address}>서울시 동작구 상도동</div>
-        <div className={styles.title}>날아라 치과에서</div>
-        <div className={styles.title}>2021년 5월 10일 수요일에</div>
+        <div className={styles.address}>{data?.dentistAddress}</div>
+        <div className={styles.title}>{data?.dentistName}에서</div>
+        {data?.writtenByCurrentUser && (
+          <div className={styles.title}>{data?.visitDate}</div>
+        )}
         <div className={styles.treatment}>
-          <span className={styles.treatmentName}>스케일링</span>
-          <span className={styles.treatmentName}>인레이 2개</span>
-          <span className={styles.treatmentName}>크라운 2개</span>
+          {categoryList.map((item, index) => (
+            <span className={styles.treatmentName}>
+              {item.category}{" "}
+              {item.category !== "스케일링" &&
+                item.category !== "잇몸" &&
+                item.count > 1 &&
+                `${item.count}개`}
+            </span>
+          ))}
         </div>
         <div className={styles.title}>치료 완료했습니다</div>
       </section>
       <section className={styles.priceSection}>
         <div className={styles.totalPrice}>
           <span className={styles.label}>총 가격</span>
-          <span className={styles.price}>1,2700000원</span>
+          <span className={styles.price}>
+            {data?.totalAmount.toLocaleString()}원
+          </span>
         </div>
         <div className={styles.detailPrices}>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>내부 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>잇몸 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>인레이 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>크라운 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>크라운 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>크라운 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>크라운 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
-          <div className={styles.priceItem}>
-            <span className={styles.label}>크라운 진료비</span>
-            <span className={styles.price}>1,2000000원</span>
-          </div>
+          {data?.treatmentList.map((item, index) => (
+            <div className={styles.priceItem}>
+              <span className={styles.label}>{item.category} 치료</span>
+              <span className={styles.price}>
+                {item.amount.toLocaleString()}원
+              </span>
+            </div>
+          ))}
         </div>
       </section>
       <section className={styles.toothSection}>
