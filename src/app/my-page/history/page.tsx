b4d35efb907a5d7d27bc658a2288d1007eced766
MoonSsunny <moonsun116@naver.com>
2024-07-21 19:51:38 +0900

    [56] feat : 나의 진료기록 작업

--- src/app/my-page/history/page.tsx
+++ src/app/my-page/history/page.tsx
@@ -1,22 +1,33 @@
+"use client";
 import styles from "./page.module.scss";
 import Header from "@/components/common/Header";
 import HistoryCard from "@/components/common/HistoryCard";
+import { useQuery } from "@tanstack/react-query";
+import { VisitData } from "../../../api/medical";
+import { fetchVisitMyData } from "@/api/medicalRecord";
+import Loading from "@/app/loading";
 
 const MyHistory = () => {
+  const { data = [], isLoading } = useQuery<VisitData[]>({
+    queryKey: ["visitMyData"],
+    queryFn: fetchVisitMyData,
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    refetchOnReconnect: true
+  });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.header}>
-        <Header title="나의 진료 기록" />
-      </div>
-      <div className={styles.cardList}>
-        {/* <HistoryCard cardType="myHistory" />
-        <HistoryCard cardType="myHistory" />
-        <HistoryCard cardType="myHistory" />
-        <HistoryCard cardType="myHistory" />
-        <HistoryCard cardType="myHistory" />
-        <HistoryCard cardType="myHistory" /> */}
-      </div>
-    </main>
+    <>
+      {isLoading && <Loading />}
+      <main className={styles.main}>
+        <div className={styles.header}>
+          <Header title="나의 진료 기록" />
+        </div>
+        <div className={styles.cardList}>
+          <HistoryCard cardType="myHistory" userData={data} />
+        </div>
+      </main>
+    </>
   );
 };
 
