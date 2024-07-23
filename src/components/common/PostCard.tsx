5a2270cad9d87aee6f61880bb163068ce76188b1
wjdtj9656 <wjdtj9656@naver.com>
2024-07-23 19:11:10 +0900

    feat : 이미지 로드 에러

--- src/components/common/PostCard.tsx
+++ src/components/common/PostCard.tsx
@@ -39,8 +39,10 @@ const PostHeader = ({ type, data }: PropsPost) => (
       <img
         src={
           data?.user?.profileImageUrl
-            ? `http://3.34.135.181:8000/upload/profileImage/${data?.user?.profileImageUrl}`
-            : "/image-default.png"
+            ? data?.user?.profileImageUrl.includes("http")
+              ? `${data?.user?.profileImageUrl}`
+              : `http://3.34.135.181:8000/upload/profileImage/${data?.user?.profileImageUrl}`
+            : `/image-default.png`
         }
         alt="user-profile"
         className={styles.profile}
