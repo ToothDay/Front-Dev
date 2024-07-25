7a2e641bec44b0e76756bfb8c9f2df3d7581f8d7
MoonSsunny <moonsun116@naver.com>
2024-07-25 15:09:23 +0900

    [70] feat : 나의 진료 부분 무한 스크롤 작업

--- src/app/my-page/history/page.tsx
+++ src/app/my-page/history/page.tsx
@@ -2,31 +2,68 @@
 import styles from "./page.module.scss";
 import Header from "@/components/common/Header";
 import HistoryCard from "@/components/common/HistoryCard";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { VisitData } from "../../../api/medical";
 import { fetchVisitMyData } from "@/api/medicalRecord";
 import Loading from "@/app/loading";
+import ScrollToTop from "@/components/common/ScrollToTop";
+import { useCallback, useEffect, useRef } from "react";
 
 const MyHistory = () => {
-  const { data = [], isLoading } = useQuery<VisitData[]>({
+  const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ["visitMyData"],
-    queryFn: fetchVisitMyData,
-    refetchOnWindowFocus: true,
-    refetchOnMount: true,
-    refetchOnReconnect: true
+    queryFn: ({ pageParam = 0 }: { pageParam: number }) =>
+      fetchVisitMyData(pageParam),
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return undefined;
+      return pages.length;
+    },
+    staleTime: 0,
+    initialPageParam: 0
   });
 
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    });
+
+    if (loadMoreRef.current) observer.observe(loadMoreRef.current);
+
+    return () => {
+      if (loadMoreRef.current) observer.unobserve(loadMoreRef.current);
+    };
+  }, [handleObserver]);
+
+  const mainRef = useRef<HTMLDivElement>(null);
   return (
     <>
       {isLoading && <Loading />}
-      <main className={styles.main}>
+      <main className={styles.main} ref={mainRef}>
         <div className={styles.header}>
           <Header title="나의 진료 기록" />
         </div>
         <div className={styles.cardList}>
-          <HistoryCard cardType="myHistory" userData={data} />
+          {data?.pages.flatMap((page, index) => (
+            <HistoryCard cardType="myHistory" userData={page} />
+          ))}
+          <div ref={loadMoreRef} />
         </div>
       </main>
+      <ScrollToTop mainRef={mainRef} />
     </>
   );
 };
