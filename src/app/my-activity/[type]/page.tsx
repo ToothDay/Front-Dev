fa60da5ebdc8bb099f665c99576b0224475faf34
MoonSsunny <moonsun116@naver.com>
2024-07-26 12:31:52 +0900

    [70] feat : 마이페이지 내가 한 글들 작업 완료

--- src/app/my-activity/[type]/page.tsx
+++ src/app/my-activity/[type]/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 import Header from "@/components/common/Header";
 import styles from "./page.module.scss";
-import PostCard from "@/components/common/PostCard";
 import NoData from "@/components/common/NoData";
+import { useMutation, useInfiniteQuery } from "@tanstack/react-query";
+import {
+  fetchCommentPostData,
+  fetchLikePostData,
+  fetchMyPostData
+} from "@/api/myPage";
+import { useEffect, useState } from "react";
+import { PostData } from "../../../api/myPage";
+import MyPostCard from "@/components/common/MyPostCard";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 
 type ActivityType = "post" | "like" | "comment";
 
@@ -22,24 +32,108 @@ const HEADER_TITLE: HeaderTitleType = {
 };
 
 const MyActivity = ({ params }: PropsPage) => {
+  const [myPost, setMyPost] = useState<PostData[]>([]);
+  const [isFetched, setIsFetched] = useState(false);
+
+  const postMutation = useMutation({
+    mutationFn: fetchMyPostData,
+    onSuccess: (data) => {
+      console.log("data____", data);
+      setMyPost(data);
+      setIsFetched(true);
+    },
+    onError: (error) => {
+      setIsFetched(true);
+    }
+  });
+
+  const likePostMutation = useMutation({
+    mutationFn: fetchLikePostData,
+    onSuccess: (data) => {
+      console.log("data_____", data);
+      setMyPost(data);
+      setIsFetched(true);
+    },
+    onError: (error) => {
+      setIsFetched(true);
+    }
+  });
+
+  const commentPostMutation = useMutation({
+    mutationFn: fetchCommentPostData,
+    onSuccess: (data) => {
+      console.log("data_____", data);
+      setMyPost(data);
+      setIsFetched(true);
+    },
+    onError: (error) => {
+      setIsFetched(true);
+    }
+  });
+
+  const getMyPost = () => {
+    try {
+      postMutation.mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getLikePost = () => {
+    try {
+      likePostMutation.mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getCommentPost = () => {
+    try {
+      commentPostMutation.mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (params.type === "like") {
+      getLikePost();
+      console.log(myPost);
+    }
+    if (params.type === "post") {
+      getMyPost();
+    }
+    if (params.type === "comment") {
+      getCommentPost();
+    }
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.header}>
-        <Header title={HEADER_TITLE[params.type]} />
-      </div>
-      <div className={styles.postList}>
-        {/* 데이터에 맞게 수정될 예정  */}
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        <PostCard type={params.type} />
-        {/* 데이터 없을때  */}
-        {/* <NoData type={params.type} /> */}
-      </div>
-    </main>
+    <>
+      {postMutation.isPending ? (
+        <LoadingSpinner />
+      ) : (
+        <main className={styles.main}>
+          <div className={styles.header}>
+            <Header title={HEADER_TITLE[params.type]} />
+          </div>
+          {!isFetched && <LoadingSpinner />}
+          <div className={styles.postList}>
+            {!postMutation.isPending && isFetched && myPost.length === 0 ? (
+              <NoData type={params.type} />
+            ) : (
+              myPost.map((post) => (
+                <MyPostCard
+                  key={post.postId}
+                  type={params.type}
+                  listData={post}
+                />
+              ))
+            )}
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
