3843bced13032126a040e2260429c939b4f321d3
MoonSsunny <moonsun116@naver.com>
2024-07-26 12:31:07 +0900

    [70] feat : 내가쓴 댓글, 좋아요, 포스트 정보 컴포넌트

---
+++ src/components/common/MyPostCard.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { postLike } from "@/api/communityApi";
+import { PostData } from "@/api/myPage";
+import styles from "@/components/common/MyPostCard.module.scss";
+import { formatYYYYMMDDTIME } from "@/util/formatDate";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { TREATMENT_LIST } from "@/constants/treatmentConstants";
+
+type PropsPost = {
+  type: "post" | "comment" | "like";
+  listData: PostData;
+  refetch?: any;
+};
+
+const MyPostCard = ({ type, listData, refetch }: PropsPost) => {
+  const [tagList, setTagList] = useState<string[]>([]);
+  const [likedByCurrentUser, setLikedByCurrentUser] = useState<boolean>(
+    listData.likedByCurrentUser ?? true
+  );
+  const [likeCount, setLikeCount] = useState(listData.likeCount || 0);
+  const [isImageError, setIsImageError] = useState<{ [key: number]: boolean }>(
+    {}
+  );
+
+  useEffect(() => {
+    if (listData.keywords.length > 0) {
+      const tags = listData.keywords.filter((keyword) => keyword !== 1);
+      const tagList = tags.map((tag) => {
+        return TREATMENT_LIST.find((treatment) => treatment.keywordId === tag)
+          ?.name;
+      });
+      setTagList(tagList as string[]);
+    }
+  }, [listData]);
+
+  const likeMutation = useMutation({
+    mutationFn: (postId: number) => postLike(postId),
+
+    onSuccess: () => {
+      setLikedByCurrentUser((prev) => !prev);
+      setLikeCount((prev) => (likedByCurrentUser ? prev - 1 : prev + 1));
+    }
+  });
+
+  const handleImageError = (postId: number) => {
+    if (!isImageError[postId]) {
+      setIsImageError((prev) => ({ ...prev, [postId]: true }));
+    }
+  };
+
+  return (
+    <div className={[styles.postWrapper, styles[`${type}Type`]].join(" ")}>
+      <div className={styles.postCard}>
+        <div className={styles.tagList}>
+          {tagList.map((tag, index) => (
+            <span key={index} className={styles.tag}>
+              #{tag}
+            </span>
+          ))}
+        </div>
+        <div className={styles.postHeader}>
+          {type !== "post" && (
+            <Image
+              src={
+                listData.user?.profileImageUrl && !isImageError[listData.postId]
+                  ? listData.user?.profileImageUrl
+                  : "/profile.svg"
+              }
+              alt="tooth"
+              width={34}
+              height={34}
+              className={styles.profileIcon}
+              loading="lazy"
+              blurDataURL="/profile.svg"
+              placeholder="blur"
+              onError={() => handleImageError(listData.postId)}
+            />
+          )}
+          <div className={styles.postInfo}>
+            <p className={styles.postTitle}>
+              {listData.title ?? "(제목 없음)"}
+            </p>
+            <div className={styles.postSubInfo}>
+              {type !== "post" && (
+                <span className={styles.nickName}>
+                  {listData.user?.username}
+                </span>
+              )}
+              <span className={styles.time}>
+                {formatYYYYMMDDTIME(listData.createDate)}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.post}>
+        <div className={styles.postContentWrapper}>
+          <p className={styles.postContent}>
+            {listData.content ?? "(내용 없음)"}
+          </p>
+        </div>
+        <div className={styles.postImage}>
+          {listData.imageUrl.map((url, index) => (
+            <Image
+              width={70}
+              height={70}
+              key={index}
+              src={process.env.IMAGE_PATH + url ?? "/profile.svg"}
+              alt="post-image"
+              className={styles.image}
+              blurDataURL="/profile.svg"
+              placeholder="blur"
+              loading="lazy"
+            />
+          ))}
+        </div>
+      </div>
+      {type !== "comment" && (
+        <div className={styles.postFooter}>
+          <span className={styles.commentNumber}>{listData.commentCount}</span>
+          <span
+            className={[
+              styles.likeNumber,
+              likedByCurrentUser ? styles.selected : ""
+            ].join(" ")}
+            onClick={(e) => {
+              e.preventDefault();
+              likeMutation.mutate(listData.postId);
+            }}
+          >
+            {likeCount}
+          </span>
+        </div>
+      )}
+      {type === "comment" && (
+        <div className={styles.comment}>
+          <span className={styles.commentTitle}>내 댓글</span>
+          <p className={styles.commentText}>
+            {listData.myComment ?? "(댓글 없음)"}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyPostCard;
