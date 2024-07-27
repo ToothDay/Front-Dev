ff83c104eb3ce3d604089e173a4fc81e0b9c4782
MoonSsunny <moonsun116@naver.com>
2024-07-27 16:33:53 +0900

    [72] feat : 수정

--- src/components/mypage/UserProfileCard.tsx
+++ src/components/mypage/UserProfileCard.tsx
@@ -3,26 +3,12 @@ import Link from "next/link";
 import styles from "./UserProfileCard.module.scss";
 import { useUserStore } from "@/stores/user";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { getProfile } from "@/api/profileApi";
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 const UserProfileCard = () => {
   const { userProfile } = useUserStore();
   const { username, profileImageUrl } = userProfile;
   const [profileImage, setProfileImage] = useState<string>(profileImageUrl);
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["getProfile"],
-    queryFn: () => getProfile(),
-    staleTime: 0
-  });
-
-  useEffect(() => {
-    setProfileImage(
-      `${process.env.NEXT_PUBLIC_IMAGE_PATH}${data?.profileImageUrl}`
-    );
-  }, [data]);
-
   return (
     <article className={styles.user}>
       <Image
