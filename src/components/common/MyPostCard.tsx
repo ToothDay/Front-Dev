3cc0429ca912f902e059ce54a12f30948c105886
MoonSsunny <moonsun116@naver.com>
2024-07-27 20:18:38 +0900

    [72] feat : 데이터 로딩스피너

--- src/components/common/MyPostCard.tsx
+++ src/components/common/MyPostCard.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { TREATMENT_LIST } from "@/constants/treatmentConstants";
 import { useRouter } from "next/navigation";
+import Loading from "@/app/loading";
 
 type PropsPost = {
   type: "post" | "comment" | "like";
@@ -28,6 +29,7 @@ const MyPostCard = ({ type, listData, refetch }: PropsPost) => {
     [key: number]: boolean;
   }>({});
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (listData.keywords.length > 0) {
@@ -62,112 +64,122 @@ const MyPostCard = ({ type, listData, refetch }: PropsPost) => {
   };
 
   const handlePost = (postId: number) => {
+    setIsLoading(true);
     router.push(`/community/post/${postId}`);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
   };
 
   return (
-    <div
-      className={[styles.postWrapper, styles[`${type}Type`]].join(" ")}
-      onClick={() => handlePost(listData.postId)}
-    >
-      <div className={styles.postCard}>
-        <div className={styles.tagList}>
-          {tagList.map((tag, index) => (
-            <span key={index} className={styles.tag}>
-              #{tag}
-            </span>
-          ))}
-        </div>
-        <div className={styles.postHeader}>
-          {type !== "post" && (
-            <Image
-              src={
-                listData.user?.profileImageUrl && !isImageError[listData.postId]
-                  ? listData.user?.profileImageUrl
-                  : "/profile.svg"
-              }
-              alt="tooth"
-              width={34}
-              height={34}
-              className={styles.profileIcon}
-              loading="lazy"
-              blurDataURL="/profile.svg"
-              placeholder="blur"
-              onError={() => handleImageError(listData.postId)}
-            />
-          )}
-          <div className={styles.postInfo}>
-            <p className={styles.postTitle}>
-              {listData.title ?? "(제목 없음)"}
-            </p>
-            <div className={styles.postSubInfo}>
-              {type !== "post" && (
-                <span className={styles.nickName}>
-                  {listData.user?.username}
-                </span>
-              )}
-              <span className={styles.time}>
-                {formatYYYYMMDDTIME(listData.createDate)}
+    <>
+      {isLoading && <Loading />}
+      <div
+        className={[styles.postWrapper, styles[`${type}Type`]].join(" ")}
+        onClick={() => handlePost(listData.postId)}
+      >
+        <div className={styles.postCard}>
+          <div className={styles.tagList}>
+            {tagList.map((tag, index) => (
+              <span key={index} className={styles.tag}>
+                #{tag}
               </span>
+            ))}
+          </div>
+          <div className={styles.postHeader}>
+            {type !== "post" && (
+              <Image
+                src={
+                  listData.user?.profileImageUrl &&
+                  !isImageError[listData.postId]
+                    ? listData.user?.profileImageUrl
+                    : "/profile.svg"
+                }
+                alt="tooth"
+                width={34}
+                height={34}
+                className={styles.profileIcon}
+                loading="lazy"
+                blurDataURL="/profile.svg"
+                placeholder="blur"
+                onError={() => handleImageError(listData.postId)}
+              />
+            )}
+            <div className={styles.postInfo}>
+              <p className={styles.postTitle}>
+                {listData.title ?? "(제목 없음)"}
+              </p>
+              <div className={styles.postSubInfo}>
+                {type !== "post" && (
+                  <span className={styles.nickName}>
+                    {listData.user?.username}
+                  </span>
+                )}
+                <span className={styles.time}>
+                  {formatYYYYMMDDTIME(listData.createDate)}
+                </span>
+              </div>
             </div>
           </div>
         </div>
+        <div className={styles.post}>
+          <div className={styles.postContentWrapper}>
+            <p className={styles.postContent}>
+              {listData.content ?? "(내용 없음)"}
+            </p>
+          </div>
+          <div className={styles.postImage}>
+            {listData.imageUrl.map((url, index) => (
+              <Image
+                width={70}
+                height={70}
+                key={index}
+                src={
+                  process.env.IMAGE_PATH + "/" + url &&
+                  !isPostImageError[listData.postId]
+                    ? process.env.IMAGE_PATH + "/" + url
+                    : "/image-default.png"
+                }
+                alt="post-image"
+                className={styles.image}
+                blurDataURL="/image-default.png"
+                placeholder="blur"
+                loading="lazy"
+                onError={() => handlePostImageError(listData.postId)}
+              />
+            ))}
+          </div>
+        </div>
+        {type !== "comment" && (
+          <div className={styles.postFooter}>
+            <span className={styles.commentNumber}>
+              {listData.commentCount}
+            </span>
+            <span
+              className={[
+                styles.likeNumber,
+                likedByCurrentUser ? styles.selected : ""
+              ].join(" ")}
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                likeMutation.mutate(listData.postId);
+              }}
+            >
+              {likeCount}
+            </span>
+          </div>
+        )}
+        {type === "comment" && (
+          <div className={styles.comment}>
+            <span className={styles.commentTitle}>내 댓글</span>
+            <p className={styles.commentText}>
+              {listData.myComment ?? "(댓글 없음)"}
+            </p>
+          </div>
+        )}
       </div>
-      <div className={styles.post}>
-        <div className={styles.postContentWrapper}>
-          <p className={styles.postContent}>
-            {listData.content ?? "(내용 없음)"}
-          </p>
-        </div>
-        <div className={styles.postImage}>
-          {listData.imageUrl.map((url, index) => (
-            <Image
-              width={70}
-              height={70}
-              key={index}
-              src={
-                process.env.IMAGE_PATH + "/" + url &&
-                !isPostImageError[listData.postId]
-                  ? process.env.IMAGE_PATH + "/" + url
-                  : "/image-default.png"
-              }
-              alt="post-image"
-              className={styles.image}
-              blurDataURL="/image-default.png"
-              placeholder="blur"
-              loading="lazy"
-              onError={() => handlePostImageError(listData.postId)}
-            />
-          ))}
-        </div>
-      </div>
-      {type !== "comment" && (
-        <div className={styles.postFooter}>
-          <span className={styles.commentNumber}>{listData.commentCount}</span>
-          <span
-            className={[
-              styles.likeNumber,
-              likedByCurrentUser ? styles.selected : ""
-            ].join(" ")}
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              likeMutation.mutate(listData.postId);
-            }}
-          >
-            {likeCount}
-          </span>
-        </div>
-      )}
-      {type === "comment" && (
-        <div className={styles.comment}>
-          <span className={styles.commentTitle}>내 댓글</span>
-          <p className={styles.commentText}>
-            {listData.myComment ?? "(댓글 없음)"}
-          </p>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
